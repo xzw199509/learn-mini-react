4a1f88afd61ad4c8e09d71e8a27c949a516b2447
xiao1995 <1050149954@qq.com>
2024-01-14 23:03:47 +0800

    requestIdleCallback 与 fiber

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ day 01
   了解可以利用vite对Jsx解析
 2、添加单元测试
  测试createElement
+
+day 02
+requestIdleCallback 与 fiber
+requestIdleCallback 利用浏览器空闲时间调用
+fiber 使用单链表结构表示数据结构，定义对dom节点遍历方式
--- core/React.js
+++ core/React.js
@@ -21,22 +21,96 @@ function createElement(type, props, ...children) {
 }
 
 function render(el, container) {
-  const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(el.type)
+  nextWorkOfUnit = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  }
+  // const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(el.type)
 
-  console.log('el', el)
-  // id class
-  Object.keys(el.props).forEach((key) => {
+  // console.log('el', el)
+  // // id class
+  // Object.keys(el.props).forEach((key) => {
+  //   if (key !== 'children') {
+  //     dom[key] = el.props[key]
+  //   }
+  // })
+
+  // const children = el.props.children
+  // children.forEach((child) => {
+  //   render(child, dom)
+  // })
+  // container.append(dom)
+}
+
+function createDom(type) {
+  return type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type)
+}
+function updateProps(dom, props) {
+  Object.keys(props).forEach((key) => {
     if (key !== 'children') {
-      dom[key] = el.props[key]
+      dom[key] = props[key]
     }
   })
-
-  const children = el.props.children
-  children.forEach((child) => {
-    render(child, dom)
-  })
-  container.append(dom)
 }
+function initChildren(fiber) {
+  const children = fiber.props.children
+  let prevChild = null
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null,
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevChild.sibling = newFiber
+    }
+    prevChild = newFiber
+  })
+}
+function perforWorkOfUnit(fiber) {
+  // 1. 创建 dom
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type))
+    fiber.parent.dom.append(dom)
+
+    // 2. 处理 props
+    updateProps(dom, fiber.props)
+  }
+
+  // 3. 转换链表 设置好指针
+  initChildren(fiber)
+
+  // 4. 返回下一个要执行的任务
+  if (fiber.child) {
+    return fiber.child
+  }
+  if (fiber.sibling) {
+    return fiber.sibling
+  }
+  console.log('perforWorkOfUnit', fiber)
+  return fiber.parent?.sibling
+}
+let workId = 1
+let nextWorkOfUnit = null
+function workLoop(deadline) {
+  workId++
+  let shouldYield = false
+  while (!shouldYield && nextWorkOfUnit) {
+    nextWorkOfUnit = perforWorkOfUnit(nextWorkOfUnit)
+
+    shouldYield = deadline.timeRemaining() < 0
+  }
+  console.log('workId:' + workId, nextWorkOfUnit)
+  requestIdleCallback(workLoop)
+}
+requestIdleCallback(workLoop)
 
 const React = {
   render,
